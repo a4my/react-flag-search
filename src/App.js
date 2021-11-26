9f975b3685fc3e847fc6bf841360d57479d333dd
Alex Fourmy <alexfourmy@gmail.com>
2021-11-26 16:48:28 +0100

    worked on different paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import NotFound from './pages/NotFound.jsx'
+import About from './pages/About.jsx'
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </Router>
   )
 }
 
