2a489414583eadbc1568e3c150252acbeaca9b10
Alex Fourmy <alexfourmy@gmail.com>
2021-11-26 16:51:25 +0100

    Created an About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>About</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        ipsa laudantium facilis reiciendis totam illo odio tenetur laborum.
+        Explicabo corporis consequuntur iure ipsam laboriosam quam dolores sit
+        cumque architecto eaque, pariatur ratione cum recusandae, aut sint
+        delectus, nam modi. Fugit quas non, dignissimos dicta culpa cumque enim
+        tenetur minus ad saepe harum voluptatibus delectus beatae quis eveniet
+        esse sequi. Ab.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus neque,
+        sunt exercitationem consequatur quasi nostrum? Sint adipisci rem
+        veritatis, impedit libero eveniet hic labore sit voluptatem nihil odio,
+        perferendis pariatur assumenda. Eum quaerat facilis architecto enim
+        ipsum assumenda impedit modi at quas minus! Ratione ab assumenda vitae
+        ea libero molestiae distinctio consectetur minima dignissimos dolore,
+        vero expedita dolor accusantium velit molestias reprehenderit
+        perspiciatis quis nesciunt praesentium adipisci sequi rem fuga obcaecati
+        laboriosam. Sequi exercitationem quae vitae cum eos minima earum?
+      </p>
+    </div>
+  )
+}
+
+export default About
