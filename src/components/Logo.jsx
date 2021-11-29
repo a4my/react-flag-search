1866fd6c0f8f3eaadd164ae3d95a213d3eae4c26
Alex Fourmy <alexfourmy@gmail.com>
2021-11-29 14:21:54 +0100

    Created a component for the app logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img src="./img/logo192.png" alt="logo" />
+      <h3>Flag Search</h3>
+    </div>
+  )
+}
